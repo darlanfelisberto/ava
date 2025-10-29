a22fb824264914ec13186256674361af56f2c559
darlan <darlanfelisberto@gmail.com>
2025-10-29 16:47:37 -0300

    .

--- Aava/src/app/app.config.ts
+++ Aava/src/app/app.config.ts
@@ -5,11 +5,18 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { routes } from './app.routes';
 import { AuthService } from './auth.service';
 import { DefaultOAuthInterceptor, OAuthStorage, provideOAuthClient } from 'angular-oauth2-oidc';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
 
     // Configura o HttpClient para usar interceptors baseados em DI
     provideHttpClient(),
