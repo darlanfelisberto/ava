831620795dd1f6f9b3476764d167ef0f0b358c85
darlan <darlanfelisberto@gmail.com>
2025-11-17 09:52:11 -0300

    template inicial

--- Aava/src/app/app.ts
+++ Aava/src/app/app.ts
@@ -5,9 +5,23 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <router-outlet />
+
+    <div class="ds-template">
+      <div class="logo">logo</div>
+      <div class="header"><h2>My Header</h2></div>
+      <div class="menu">
+
+      </div>
+      <div class="content">
+        <router-outlet />
+      </div>
+    </div>
   `,
   styles: `
+    nav{
+      background-color: #373737;
+      width: 200px;
+    }
   `
 })
 export class App {
